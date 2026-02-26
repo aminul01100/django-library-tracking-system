29bf4f3420e085146bb5906f85d83cd5916f151a
Md. Aminul Islam <mdaminul.bs23@gmail.com>
2026-02-27 03:01:12 +0600

    1. Python Core Skills -> completed

--- core_skills.py
+++ core_skills.py
@@ -1,6 +1,16 @@
 import random
+
 # rand_list =
+def get_10_random_numbers() -> list[int]:
+    return [random.randint(1, 20) for _ in range(10)]
 
 # list_comprehension_below_10 =
+def filter_numbers_below_10_using_comprehension(number_list: list[int]) -> list[int]:
+    return [number for number in number_list if number > 10]
 
-# list_comprehension_below_10 =
+# list_comprehension_using_filter =
+def filter_numbers_below_10_using_filter(number_list: list[int]) -> list[int]:
+    def is_greater_than_10(n):
+        return n > 10
+    filtered_numbers = list(filter(is_greater_than_10, number_list))
+    return filtered_numbers
